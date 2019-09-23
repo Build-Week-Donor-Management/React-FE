243543cf72d8b55c687b200eaaace9337062bdff
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-23 14:08:04 -0700

    building log in and sign up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+//components
 import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <p>render test</p>
       <SignUp />
+      <Login />
     </div>
   );
 }
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,25 +1,31 @@
 // app=>login=> dashboard=>create a campaign =>or add a donor => dashboard
 import React from "react";
 import axiosWithAuth from "../utilities/axiosWithAuth";
-import { Form } from "semantic-ui-react";
+import { Segment, Form, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const login = () => {
   return (
     <div className="login-wrapper">
-      <div className="login-cta">
-        <h1>Please Log</h1>
-      </div>
-      <Form>
-        <Form.Field>
-          <label>Email</label>
-          <input type="email" placeholder="Email" />
-        </Form.Field>
-        <Form.Field>
-          <label>Password</label>
-          <input type="password" placeholder="Register your password" />
-        </Form.Field>
-        <Button>Log In</Button>
-      </Form>
+      <Segment raised compact>
+        <div className="login-cta">
+          <h1>Please Log In</h1>
+        </div>
+        <Form>
+          <Form.Field>
+            <label>Email</label>
+            <input type="email" placeholder="Email" />
+          </Form.Field>
+          <Form.Field>
+            <label>Password</label>
+            <input type="password" placeholder="Register your password" />
+          </Form.Field>
+          <Button>Log In</Button>
+          <Link to="/signup">
+            <p>Need to create an Account?</p>
+          </Link>
+        </Form>
+      </Segment>
     </div>
   );
 };
