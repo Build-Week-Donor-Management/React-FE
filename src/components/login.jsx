8dd8b712538ac3cf12e715fd5e1bfde3fb49ea59
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-23 13:54:03 -0700

    browser router add to index

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,8 +1,27 @@
 // app=>login=> dashboard=>create a campaign =>or add a donor => dashboard
 import React from "react";
 import axiosWithAuth from "../utilities/axiosWithAuth";
+import { Form } from "semantic-ui-react";
+
 const login = () => {
-  return <div></div>;
+  return (
+    <div className="login-wrapper">
+      <div className="login-cta">
+        <h1>Please Log</h1>
+      </div>
+      <Form>
+        <Form.Field>
+          <label>Email</label>
+          <input type="email" placeholder="Email" />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input type="password" placeholder="Register your password" />
+        </Form.Field>
+        <Button>Log In</Button>
+      </Form>
+    </div>
+  );
 };
 
 export default login;
