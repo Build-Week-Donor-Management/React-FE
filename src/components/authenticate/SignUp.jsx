188220333f4f0a483ce4b4e8a5bc7687ba3d4a9a
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-23 16:05:04 -0700

    wip signup form

--- src/components/authenticate/SignUp.jsx
+++ src/components/authenticate/SignUp.jsx
@@ -1,23 +1,58 @@
 import React, { useState } from "react";
-import axiosWithAuth from "../../utilities/axiosWithAuth";
+// import { axiosWithAuth } from "../../utilities/axiosWithAuth";
+import axios from "axios";
 import { Segment, Form, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  const [state, setState] = useState({});
+  const [state, setState] = useState({
+    first_name: "",
+    last_name: "",
+    organization: "",
+    email: "",
+    password: ""
+  });
+
+  const changeHandler = event => {
+    setState({
+      ...state,
+      [event.target.name]: event.value
+    });
+    const submitHandler = (event, state) => {
+      event.preventDefault();
+      axios
+        .post("/signup", state)
+        .then(res => {
+          props.history.push("/login");
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      setState({
+        first_name: "",
+        last_name: "",
+        organization: "",
+        email: "",
+        password: ""
+      });
+    };
+  };
   return (
     <div className="signUp-wrapper">
       <Segment raised compact>
         <div className="signup-cta">
           <h1>Sign Up Today!</h1>
         </div>
-        <Form>
+        <Form onSubmit={event => submitHandler(event, state)}>
           <Form.Field>
             <label>First Name</label>
             <input
               type="text"
               name="first_name"
               placeholder="Register your First Name"
+              value={state.first_name}
+              onChange={changeHandler}
+              required
             />
           </Form.Field>
           <Form.Field>
@@ -26,6 +61,9 @@ const SignUp = () => {
               type="text"
               name="last_name"
               placeholder="Register your Last Name"
+              value={state.last_name}
+              onChange={changeHandler}
+              required
             />
           </Form.Field>
           <Form.Field>
@@ -34,6 +72,9 @@ const SignUp = () => {
               type="text"
               name="organization"
               placeholder="Register an Organization"
+              value={state.organization}
+              onChange={changeHandler}
+              required
             />
           </Form.Field>
           <Form.Field>
@@ -42,6 +83,9 @@ const SignUp = () => {
               type="email"
               name="email"
               placeholder="Register your Email"
+              value={state.email}
+              onChange={changeHandler}
+              required
             />
           </Form.Field>
           <Form.Field>
@@ -50,9 +94,12 @@ const SignUp = () => {
               type="password"
               name="password"
               placeholder="Register your password"
+              value={state.password}
+              onChange={changeHandler}
+              required
             />
           </Form.Field>
-          <Button>Sign Up</Button>
+          <Button>Submit</Button>
           <Link to="/login">
             <p>Already have an Account?</p>
           </Link>
