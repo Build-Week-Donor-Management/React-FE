04e3a5fad683413b4cf751f8879089b38a010297
Daniel Jones <daniel.jones2000@gmail.com>
2019-09-24 20:54:10 -0700

    creating layout for display cards

--- src/components/CampaignCard.jsx
+++ src/components/CampaignCard.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const CampaignCard = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h3>Campaign Title</h3>
+      <p>Description</p>
+      <p>Donation Goal</p>
+    </div>
+  );
 };
 
 export default CampaignCard;
