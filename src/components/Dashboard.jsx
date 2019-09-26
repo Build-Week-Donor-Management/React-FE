570ff53c523663f16e933b50d6aa57edf0ecd10d
Oluwajoba <oluwajobabello@gmail.com>
2019-09-26 06:13:15 +0100

    add fake data from reqres.in/api/users to file / Dashboard2.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,24 +1,138 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+
+
+const data = [
+  {
+    id: 1,
+    email: "george.bluth@reqres.in",
+    first_name: "George",
+    last_name: "Bluth",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
+  },
+  {
+    id: 2,
+    email: "janet.weaver@reqres.in",
+    first_name: "Janet",
+    last_name: "Weaver",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
+  },
+  {
+    id: 3,
+    email: "emma.wong@reqres.in",
+    first_name: "Emma",
+    last_name: "Wong",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
+  },
+  {
+    id: 4,
+    email: "eve.holt@reqres.in",
+    first_name: "Eve",
+    last_name: "Holt",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg"
+  },
+  {
+    id: 5,
+    email: "charles.morris@reqres.in",
+    first_name: "Charles",
+    last_name: "Morris",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg"
+  },
+  {
+    id: 6,
+    email: "tracey.ramos@reqres.in",
+    first_name: "Tracey",
+    last_name: "Ramos",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg"
+  },
+  {
+    id: 7,
+    email: "michael.lawson@reqres.in",
+    first_name: "Michael",
+    last_name: "Lawson",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"
+  },
+  {
+    id: 8,
+    email: "lindsay.ferguson@reqres.in",
+    first_name: "Lindsay",
+    last_name: "Ferguson",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg"
+  },
+  {
+    id: 9,
+    email: "tobias.funke@reqres.in",
+    first_name: "Tobias",
+    last_name: "Funke",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg"
+  },
+  {
+    id: 10,
+    email: "byron.fields@reqres.in",
+    first_name: "Byron",
+    last_name: "Fields",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/russoedu/128.jpg"
+  },
+  {
+    id: 11,
+    email: "george.edwards@reqres.in",
+    first_name: "George",
+    last_name: "Edwards",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/mrmoiree/128.jpg"
+  },
+  {
+    id: 12,
+    email: "rachel.howell@reqres.in",
+    first_name: "Rachel",
+    last_name: "Howell",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/hebertialmeida/128.jpg"
+  }
+]
+
+
+
+
 function Home(params) {
   return <h1>Home Page</h1>;
 }
 function Campaigns(params) {
-  return <h1>Campaign Page</h1>;
+  return (
+    <div>
+      {}
+    </div>
+  );
 }
-function Donor(params) {
+function Donors(params) {
   return <h1>Donor Page</h1>;
+}
+function Logout(params) {
+  return <h1>Logout Page</h1>;
 }
 function DashBoard(params) {
   return (
     <BrowserRouter>
-      <Link to="/">Home</Link>
-      <Link to="/campaign">Campaigns</Link>
-      <Link to="/donor">Donor</Link>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/campaign">Campaigns</Link>
+          </li>
+          <li>
+            <Link to="/donor">Donors</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+        </ul>
+      </div>
 
       <Route exact path="/" component={Home} />
       <Route path="/campaign" component={Campaigns} />
-      <Route path="/donor" component={Donor} />
+      <Route path="/donor" component={Donors} />
+      <Route path="/logot" component={Logout} />
+
     </BrowserRouter>
   );
 }
