6e93339e597ecae90b5831fcebd546633c93892d
Oluwajoba <oluwajobabello@gmail.com>
2019-09-26 04:38:58 +0100

    change Dashboard.jsx to Dashboard2.jsx / fix 'can't resolve error' / App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import SignUp from "./components/authenticate/SignUp";
 import LogIn from "./components/authenticate/Login";
 import { UserContext } from "./components/Context/UserContext";
-import Dashboard from "./components/Dashboard";
+import Dashboard2 from "./components/Dashboard2";
 import PrivateRoute from "./components/authenticate/PrivateRoute";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/signup" render={props => <SignUp {...props} />} />
           <Route path="/login" render={props => <LogIn {...props} />} />
-          <PrivateRoute path="/" component={Dashboard} />
+          <PrivateRoute path="/" component={Dashboard2} />
         </Switch>
       </UserContext.Provider>
     </div>
