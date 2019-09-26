55875254c3b1ada0074f6c8fa302f5dd7338f807
Oluwajoba <oluwajobabello@gmail.com>
2019-09-26 09:36:52 +0100

    create CampaignCard / Dashboard2.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -95,6 +95,8 @@ const data = [
 function Home(params) {
   return <h1>Home Page</h1>;
 }
+
+
 function Campaigns(props) {
   return (
     <div>
@@ -103,17 +105,52 @@ function Campaigns(props) {
         <div keys={campaign.id}>
           <ul>
             <li>
-              <h3>Our campaign name is {campaign.last_name}  </h3>
+              {/* <h3>Our campaign name is {campaign.last_name}  </h3> */}
+              <h3 onClick={() => props.history.push(`/campaign/${campaign.id}/email`)}>Our campaign name is {campaign.last_name} </h3>
               <p>Managed by {campaign.first_name}  </p>
               <p>({campaign.email})</p>
             </li>
           </ul>
         </div>
-
-           ))}
+      ))}
     </div>
   );
 }
+function CampaignCardEmail(props) {
+  return(
+    <p>props.campaigncard.email</p>
+  )
+}
+function CampaignCard(props) {
+  const campaigncard = props.campaignList.find(
+    cCard => cCard.id === parseInt(props.match.params.id, 10)
+  );
+
+  return (
+    <div>
+      <h1>Campaign Card</h1>
+      <img src={campaigncard.avatar} alt={campaigncard.first_name} />
+      <div>
+          <h1>{campaigncard.first_name} {campaigncard.last_name}</h1>
+          <h4>({campaigncard.email})</h4>
+
+          <nav>
+            <Link to={`/campaign/${campaigncard.id}/email`}>Email</Link>
+          </nav>
+          <Route
+            path="/campaign/:id/email"
+            render={props => (
+              <CampaignCardEmail {...props} campaigncard={campaigncard} />
+            )}
+          />
+          
+        </div>
+    </div>
+  );
+}
+
+
+
 function Donors(props) {
 
   return (
@@ -156,11 +193,18 @@ function DashBoard(params) {
 
       <Route exact path="/" component={Home} />
       <Route path="/campaign"
-      render={props => (
-        <Campaigns {...props} campaignList={data} />
-      ) }
+        render={props => (
+          <Campaigns {...props} campaignList={data} />
+        )}
       // component={Campaigns}
       />
+      <Route
+        path="/campaign/:id"
+        render={props => (
+          <CampaignCard {...props} campaignList={data} />
+        )}
+      />
+
       <Route
         path="/donor"
         render={props => (
